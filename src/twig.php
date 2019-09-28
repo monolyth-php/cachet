7fbfef40c96464443730463138e27c224bfe208c
Marijn Ophorst <marijn@monomelodies.nl>
2019-09-28 16:26:38 +0200

    setup twig helper, but on second thoughts: lets put this in a static class method

---
+++ src/twig.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Monolyth\Cachet;
+
+use Twig\Environment;
+use Twig\TwigFunction;
+use Monolyth\Envy\Enviroment;
+
+/**
+ * Helper function to quickly add the versioning function to your Twig
+ * enviroment. Not using Twig? Fine, write your own based on this :)
+ *
+ * @param string $versions Location of the versions file (relative to current
+ *  working directory).
+ * @param Twig\Environment $twig
+ */
+return function (string $versions, Environment $twig) : void {
+    if (class_exists(Environment::class)) {
+        $env = Environment::instance();
+    }
+    $twig->addFunction(new TwigFunction('version', function ($file) use ($env) {
+        if (!isset($env) || !$env->prod) {
+            return $file;
+        }
+        static $versions;
+        if (!isset($versions)) {
+            $versions = json_decode(file_get_contents(dirname(__DIR__).'/Versions.json'), true);
+        }
+        $file = preg_replace('@^/@', '', $file);
+        return preg_replace('@\.(css|js)$@', ".{$versions[$file]}.\\1", "/$file");
+    }));
+};
+
