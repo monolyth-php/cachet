26d3109cb11b53a1b113087882a0c415107a89d6
Marijn Ophorst <marijn@monomelodies.nl>
2019-09-29 15:10:05 +0200

    add docs, and also write back the new hashes so they actually get updated

--- src/Command.php
+++ src/Command.php
@@ -16,19 +16,26 @@ class Command extends Cliff\Command
      * The file extension is typically `js` or `css`, but in theory any
      * extension is allowed. The subdirectory can be specified (e.g. `styles` or
      * `scripts` but defaults to the file extension.
+     *
+     * @param string $versionFile
+     * @param string $public
+     * @param string $extension
+     * @param string $subdir Optional, defaults to the value of `$extension`.
+     * @return void
      */
-    public function __invoke(string $versions, string $public, string $extension, string $subdir = null) : void
+    public function __invoke(string $versionFile, string $public, string $extension, string $subdir = null) : void
     {
         $subdir = $subdir ?? $extension;
-        $versions = json_decode(file_get_contents(getcwd()."/$versions"), true);
-        foreach ($versions as $file => $hash) {
+        $versions = json_decode(file_get_contents(getcwd()."/$versionFile"), true);
+        foreach ($versions as $file => &$hash) {
             // The versions file has _all_ versioned files, but we only want to
             // bust those that have been asked for in the CLI arguments.
             if (!preg_match("@^$subdir/@", $file)) {
                 continue;
             }
 
-            $new = preg_replace("@$subdir/(.*?)\.(css|js)$@", "\\1.$hash.\\2", $file);
+            $newHash = substr(sha1(file_get_contents(getcwd()."/$public/$file")), 0, 8);
+            $new = preg_replace("@$subdir/(.*?)\.(css|js)$@", "\\1.$newHash.\\2", $file);
             $glob = preg_replace("@\.($extension)$@", '.*.\\1', $file);
             $files = glob(getcwd()."/$public/$glob");
             foreach ($files as $existingFile) {
@@ -39,7 +46,9 @@ class Command extends Cliff\Command
             chdir(getcwd()."/$public/$subdir");
             symlink($old, $new);
             chdir($olddir);
+            $hash = $newHash;
         }
+        file_put_contents(getcwd()."/$versionFile", json_encode($versions));
     }
 }
 
