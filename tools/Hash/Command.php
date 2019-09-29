d7429aeb57299f068249ca9bec5f44905b6cff04
Marijn Ophorst <marijn@monomelodies.nl>
2019-09-29 09:54:15 +0200

    hashing tool

---
+++ tools/Hash/Command.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tools\Hash;
+
+use Monolyth\Cliff;
+
+/**
+ * Helper command to generate a hash.
+ */
+class Command extends Cliff\Command
+{
+    /**
+     * Generate the hash for the given file.
+     *
+     * @param string $file Relative to current working directory.
+     */
+    public function __invoke(string $file) : void
+    {
+        $hash = md5(file_get_contents(getcwd()."/$file"));
+        echo "\n\nHash: ".substr($hash, 0, 8)."\n\n";
+    }
+}
+
