4c2fe4762b762ee1d89415334c7c7d075b3bb8ba
Marijn Ophorst <marijn@monomelodies.nl>
2019-09-29 15:46:53 +0200

    use the new hash

--- tests/command.php
+++ tests/command.php
@@ -8,14 +8,14 @@ return function () : Generator {
     /** By default, we can cache-bust assets where extension equals subdirectory */
     yield function () use ($object) {
         $object->__invoke('tests/files/Version.json', 'tests/files/public', 'js');
-        assert(file_exists('tests/files/public/js/test.9ed29936.js'));
+        assert(file_exists('tests/files/public/js/test.d02bc52d.js'));
         assert(!file_exists('tests/files/public/js/test.dummy.js'));
         exec("cd tests/files/public/js && ln -s test.js test.dummy.js && cd -");
     };
     /** We can also override the subdirectory to something of our choice */
     yield function () use ($object) {
         $object->__invoke('tests/files/Version.json', 'tests/files/public', 'js', 'scripts');
-        assert(file_exists('tests/files/public/scripts/test.9ed29936.js'));
+        assert(file_exists('tests/files/public/scripts/test.d02bc52d.js'));
         assert(!file_exists('tests/files/public/scripts/test.dummy.js'));
         exec("cd tests/files/public/scripts && ln -s test.js test.dummy.js && cd -");
     };
