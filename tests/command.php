55e01a00d13befd75f8907ea760090f03392ffa5
Marijn Ophorst <marijn@monomelodies.nl>
2019-09-29 10:08:34 +0200

    actually implement tests

--- tests/command.php
+++ tests/command.php
@@ -7,13 +7,17 @@ return function () : Generator {
     $object = Wrapper::createObject(Monolyth\Cachet\Command::class);
     /** By default, we can cache-bust assets where extension equals subdirectory */
     yield function () use ($object) {
-        $result = $object->__invoke('tests/files/Version.json', 'tests/files/public', 'js');
-        assert(true);
+        $object->__invoke('tests/files/Version.json', 'tests/files/public', 'js');
+        assert(file_exists('tests/files/public/js/test.9ed29936.js'));
+        assert(!file_exists('tests/files/public/js/test.dummy.js'));
+        exec("cd tests/files/public/js && ln -s test.js test.dummy.js && cd -");
     };
     /** We can also override the subdirectory to something of our choice */
     yield function () use ($object) {
-        $result = $object->__invoke('tests/files/Version.json', 'tests/files/public', 'js', 'scripts');
-        assert(true);
+        $object->__invoke('tests/files/Version.json', 'tests/files/public', 'js', 'scripts');
+        assert(file_exists('tests/files/public/scripts/test.9ed29936.js'));
+        assert(!file_exists('tests/files/public/scripts/test.dummy.js'));
+        exec("cd tests/files/public/scripts && ln -s test.js test.dummy.js && cd -");
     };
 
 };
